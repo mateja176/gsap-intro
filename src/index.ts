f1fa9ff7d34361af29b9bcd16c3dc9c8295d0e42
Mateja Petrovic <mateja176@gmail.com>
2019-05-06 21:40:48 +0200

    adds basic example of using gsap

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { Power2, TimelineMax } from 'gsap';
 
-const tween = new TimelineMax();
+document.querySelector('span').onclick = () => {
+  const t1 = new TimelineMax();
 
-tween.from('nav', 1, {
-  x: 100,
-  display: 'none',
-  ease: Power2.easeOut,
-});
+  t1.from('div', 1, {
+    height: '0',
+    ease: Power2.easeInOut,
+  });
+
+  t1.to('div', 1, {
+    height: '100%',
+    ease: Power2.easeInOut,
+  });
+};
